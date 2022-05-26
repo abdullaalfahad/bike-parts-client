c1e0182567bd2bae2fb4cbfaba79434a6025c3a2
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-26 22:37:17 +0600

    professionals fetch from database

--- src/Pages/Home/Professionals.js
+++ src/Pages/Home/Professionals.js
@@ -1,30 +1,14 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 import Professional from './Professional';
 
 const Contact = () => {
-    const professionals = [
-        {
-            id: 1,
-            name: 'Geoffrey Smith',
-            designation: 'Engineers',
-            img: 'https://i.ibb.co/tDfm2R2/professionals-01.jpg',
-            description: 'Engineers, as practitioners of engineering, are professionals who invent, design, analyze, build and test machines, complex systems, structures, gadgets and materials to fulfill functional objectives and requirements while considering the limitations imposed by practicality, regulation, safety and cost.'
-        },
-        {
-            id: 2,
-            name: 'Paul Goto',
-            designation: 'Mechanic',
-            img: 'https://i.ibb.co/1LxcN3T/professionals-02.jpg',
-            description: 'Automotive technician have many trades within. Some may specialize in the electrical diagnosis, while others may specialize in the mechanical aspects. Other mechanical areas include: brakes and steering, suspension, automatic or manual transmission, engine repairs, diagnosing customer complaints.'
-        },
-        {
-            id: 3,
-            name: 'Geoffrey Cors',
-            designation: 'Engineers',
-            img: 'https://i.ibb.co/tDfm2R2/professionals-01.jpg',
-            description: 'Engineers, as practitioners of engineering, are professionals who invent, design, analyze, build and test machines, complex systems, structures, gadgets and materials to fulfill functional objectives and requirements while considering the limitations imposed by practicality, regulation, safety and cost.'
-        }
-    ]
+    const { data: professionals, isLoading, refetch } = useQuery('professionals', () => fetch('https://vast-dawn-74828.herokuapp.com/professionals').then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
 
     return (
         <div className='w-11/12 lg:w-11/12 mx-auto text-primary'>
@@ -32,7 +16,7 @@ const Contact = () => {
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
                 {
                     professionals.map(professional => <Professional
-                        key={professional.id} professional={professional}
+                        key={professional._id} professional={professional}
                     ></Professional>)
                 }
             </div>
